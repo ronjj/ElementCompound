dfa86566da1ed46a39bb19f65c3d9e6eaba95959
ronjj <ronaldjabouin2004@gmail.com>
2021-05-05 16:54:45 -0400

    changed look of image in inventorylistcellview

--- Element Compound/Inventory Screen/InventoryListCellView.swift
+++ Element Compound/Inventory Screen/InventoryListCellView.swift
@@ -16,10 +16,9 @@ struct InventoryListCellView: View {
     var body: some View {
         HStack(spacing: 20) {
             Image(image)
-                .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 100, height: 100)
-                .cornerRadius(10)
+                .frame(width: 120, height: 90)
+                .cornerRadius(8)
                 
             VStack(alignment: .leading, spacing: 5) {
                 Text(title)
