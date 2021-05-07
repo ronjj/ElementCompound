31c64f1d3456ad03f3316f8c2ce217ccd5911255
ronjj <ronaldjabouin2004@gmail.com>
2021-05-06 21:57:12 -0400

    added the description for more items

--- Element Compound/Inventory Screen/InventoryItems.swift
+++ Element Compound/Inventory Screen/InventoryItems.swift
@@ -69,26 +69,37 @@ struct MockData {
         
         CameraItem(name: "Osmo Pocket", shortDescription: "Compact action camera", longDescription: "The Osmo Pocket provides 1080p video in a super-compact camera, mounted on a three-axis gimbal. The Osmo Pocket is a useful tool for mobile shots, time-lapses, and panoramas. Its simplistic design allows users to store the Osmo easily and record video seamlessly", imageURL: "questionmark"),
         
-        CameraItem(name: "Osmo Pocket Gen 2", shortDescription: "Versatile action camera", longDescription: "Osmo pocket 2 keeps the same compact design while adding new features and accessories to the older design. The Osmo Pocket 2 comes with multiple bases, such as a standard base, or a mini tripod. The Osmo Pocket 2 adds audio capabilities to its old video modes, making it the perfect camera for mobile interviews.", imageURL: "questionmark")
+        CameraItem(name: "Osmo Pocket Gen 2", shortDescription: "Versatile action camera", longDescription: "Osmo pocket 2 keeps the same compact design while adding new features and accessories to the older design. The Osmo Pocket 2 comes with multiple bases, such as a standard base, or a mini tripod. The Osmo Pocket 2 adds audio capabilities to its old video modes, making it the perfect camera for mobile interviews.", imageURL: "questionmark"),
+        
+        CameraItem(name: "Lenses", shortDescription: "An array of camera accessories available for club use.", longDescription: "Element has a wide range of lenses, from standard video lenses, to fish eye lenses. With a wide range of focal lengths, our inventory has the right accessory for most video projects.", imageURL: "questionmark")
         
     ]
     
     
     static let computers = [
-        ComputerItem(name: "iMac Pro", shortDescription: "", longDescription: "", imageURL: "questionmark")    ]
+        ComputerItem(name: "iMac Pro", shortDescription: "The two desktop computers that the club uses", longDescription: "", imageURL: "questionmark")    ]
     
     
     static let audios = [
-        AudioItem(name: "RODE CasterPro", shortDescription: "", longDescription: "", imageURL: "questionmark"),
-        AudioItem(name: "RODE PodMic", shortDescription: "", longDescription: "", imageURL: "questionmark"),
-        AudioItem(name: "RODE Lapel Mic", shortDescription: "", longDescription: "", imageURL: "questionmark"),
-        AudioItem(name: "Shure MV7", shortDescription: "", longDescription: "", imageURL: "questionmark"),
-        AudioItem(name: "Shure SM7b", shortDescription: "", longDescription: "", imageURL: "questionmark"),
-        AudioItem(name: "Shotgun Mic", shortDescription: "", longDescription: "", imageURL: "questionmark"),
-        AudioItem(name: "ZOOM", shortDescription: "", longDescription: "", imageURL: "questionmark"),
-        AudioItem(name: "XLR Cable", shortDescription: "", longDescription: "", imageURL: "questionmark"),
-        AudioItem(name: "Headphones", shortDescription: "", longDescription: "", imageURL: "questionmark"),
-        AudioItem(name: "MixPre", shortDescription: "", longDescription: "", imageURL: "questionmark")
+        AudioItem(name: "RODE CasterPro", shortDescription: "Premium Podcast sound mixer used for recording podcastss.", longDescription: "The RODECaster Pro is the premiere audio device for recording podcast audio. Element members have access to the powerful sound mixer to record multiple audio tracks simultaneously while adding audio effects live.", imageURL: "questionmark"),
+        
+        AudioItem(name: "RODE PodMic", shortDescription: "Professional level Podcast microphones", longDescription: "The RODE PodMic is a high quality microphone for podcast recordings. It delivers crisp, clean audio for the highest audio quality.", imageURL: "questionmark"),
+        
+        AudioItem(name: "RODE Lapel Mic", shortDescription: "Personal microphones.", longDescription: "RODE lapel mics are the best audio option for capturing audio for a specific subject. Maintaining the", imageURL: "questionmark"),
+        
+        AudioItem(name: "Shure MV7", shortDescription: "Premium Podcast Microphone", longDescription: "The Shure MV7 is our most powerful podcast microphone. The MV7 delivers the best audio quality imaginable to give you a product on par with the pros.", imageURL: "questionmark"),
+        
+        AudioItem(name: "Shure SM7b", shortDescription: "Standard Podcast Microphone", longDescription: "The Shure SM7b is our standard podcast microphone, delivering high quality audio. The SM7b performs the regular tasks required to maintain a successful podcast.", imageURL: "questionmark"),
+        
+        AudioItem(name: "Shotgun Mic", shortDescription: "Professional filmmaking microphones.", longDescription: "Our shotgun mics are the same microphones thousands of professional filmmakers use to record audio for films and interviews. Members get to use these microphones to get a hands on experience with real world filmmaking tools.", imageURL: "questionmark"),
+        
+        AudioItem(name: "ZOOM", shortDescription: "Portable audio recorder.", longDescription: "The two ZOOM audio recorders we have are portable enough to be mobile, while also providing the high quality recording we need. Small enough to fit in your backpack, the ZOOMs have four XLR ports and an external microphone. Thanks to its versatility, the ZOOM is our go-to recorder.", imageURL: "questionmark"),
+        
+        AudioItem(name: "XLR Cable", shortDescription: "XLR multi-use cable", longDescription: "A regular XLR calbe, often used for a mutltitude of audio equipment", imageURL: "questionmark"),
+        
+        AudioItem(name: "Headphones", shortDescription: " AudioTechnica brand headphones. ", longDescription: " Our over ear headphones are high quality audio devices. Approved for podcast recording and sound mixing, you won’t miss a beat with these.", imageURL: "questionmark"),
+        
+        AudioItem(name: "MixPre", shortDescription: "Professional audio recorder for filmmaking", longDescription: "The MixPre is a recording device that delivers professional audio quality for filmmaking. With more tracks and easy sound manipulation, the MixPre is the best audio device for filmmaking.", imageURL: "questionmark")
     ]
     
     
