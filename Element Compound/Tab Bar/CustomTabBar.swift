ffe49592b82ac195a37e5953cd53d29c51cfb3f6
ronjj <ronaldjabouin2004@gmail.com>
2021-06-10 16:03:14 -0400

    updated to Xcode 13 beta. Added custom tab bar and used .material for the background of it

---
+++ Element Compound/Tab Bar/CustomTabBar.swift
@@ -0,0 +1,210 @@
+//
+//  CustomTabBar.swift
+//  Element Compound
+//
+//  Created by Ronald Jabouin on 5/19/21.
+//
+
+import SwiftUI
+
+
+struct CustomTabBar: View {
+    
+    @Binding var index: Int
+    var body: some View{
+        
+        VStack{
+            
+            Spacer()
+            
+            HStack{
+                
+                Button(action: {
+                    self.index = 0
+                }, label: {
+                    VStack{
+                        if self.index != 0{
+                            Image(systemName: "house")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(.bginv)
+                                .padding()
+                        }
+                        else{
+                            Image(systemName: "house.fill")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(.bginv)
+                                .padding()
+                        }
+                        
+                    }
+                    
+                })
+                
+                Button(action: {
+                    self.index = 1
+                }, label: {
+                    VStack{
+                        if self.index != 1{
+                            Image(systemName: "list.bullet")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(.bginv)
+                                .padding()
+                        }
+                        else{
+                            Image(systemName: "list.bullet")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(.bginv)
+                                .padding()
+                        }
+                        
+                    }
+                })
+                
+                Button(action: {
+                    self.index = 2
+                }, label: {
+                    VStack{
+                        if self.index != 2{
+                            Image(systemName: "gear")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(.bginv)
+                                .padding()
+                        }
+                        else{
+                            Image(systemName: "gear")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(.bginv)
+                                .padding()
+                        }
+                        
+                    }
+                })
+                
+                Button(action: {
+                    self.index = 3
+                }, label: {
+                    VStack{
+                        if self.index != 3{
+                            Image(systemName: "note.text")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(.bginv)
+                                .padding()
+                        }
+                        else{
+                            Image(systemName: "note.text")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(.bginv)
+                                .padding()
+                        }
+                        
+                    }
+                })
+                
+                Button(action: {
+                    self.index = 4
+                }, label: {
+                    VStack{
+                        if self.index != 4{
+                            Image(systemName: "folder")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(.bginv)
+                                .padding()
+                        }
+                        else{
+                            Image(systemName: "folder.fill")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(.bginv)
+                                .padding()
+                        }
+                        
+                    }
+                })
+                
+            }
+            .padding()
+            .background(.regularMaterial)
+            .cornerRadius(20)
+            .shadow(radius: 30)
+            .animation(.default)
+        }
+        
+    }
+}
+
+
+
+
+//struct CustomTabBar: View {
+//
+//    @State var selectedTab = "home"
+//    @State var edge = UIApplication.shared.windows.first?.safeAreaInsets
+//
+//    var body: some View {
+//        ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)){
+//            TabView(selection: $selectedTab){
+//                HomeView2()
+//                    .tag("home")
+//
+//                InventoryListView(camera: MockData.sampleCamera, computer: MockData.sampleComputer, audio: MockData.sampleAudio, misc: MockData.sampleMisc)
+//                    .tag("inventory")
+//
+////                ProjectsView()
+//                   // .tag("projects")
+//
+//             //   CoursesView()
+//                  //  .tag("courses")
+//
+//                SettingsView()
+//                    .tag("settings")
+//
+//            }
+//            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+//            .ignoresSafeArea(.all,edges: .bottom)
+//
+//            HStack(spacing: 0){
+//                ForEach(tabs,id: \.self) { image in
+//                    TabButton(image: image, selectedTab: $selectedTab)
+//                    if image != tabs.last {
+//                        Spacer(minLength: 0)
+//                    }
+//                }
+//            }
+//            .padding(.horizontal, 25)
+//            .padding(.vertical, 5)
+//            .background(Color.white)
+//            .clipShape(Capsule())
+//            .shadow(color: Color.black.opacity(0.15), radius: 5, x: 5, y: 5)
+//            .shadow(color: Color.black.opacity(0.15), radius: 5, x: 5, y: 5)
+//            .padding(.horizontal)
+//            .padding(.bottom,edge!.bottom == 0 ? 20: 0 )
+//        }
+//        .ignoresSafeArea(.keyboard, edges: .bottom)
+//        .background(Color.black.opacity(0.05).ignoresSafeArea(.all, edges: .all))
+//    }
+//}
+//
+//var tabs = ["home", "inventory", "settings"]
+//
+//struct TabButton: View{
+//    var image: String
+//    @Binding var selectedTab: String
+//
+//    var body: some View{
+//        Button(action: {selectedTab = image}) {
+//            Image(image)
+//                .renderingMode(.template)
+//                .foregroundColor(selectedTab == image ? Color("tab") : Color.black.opacity(0.4))
+//                .padding()
+//        }
+//    }
+//}
---
+++ Element Compound/Tab Bar/TabView.swift
@@ -0,0 +1,81 @@
+////
+////  TabView.swift
+////  Element Compound
+////
+////  Created by Ronald Jabouin on 4/16/21.
+////
+//
+import SwiftUI
+
+struct TabViewItem: View {
+    @State var index = 0
+    var body: some View {
+        
+        VStack{
+            ZStack{
+                if self.index == 0{
+                    HomeView2()
+                }
+                else if self.index == 1{
+                    InventoryListView(camera: MockData.sampleCamera, computer: MockData.sampleComputer, audio: MockData.sampleAudio, misc: MockData.sampleMisc)
+                }
+                else if self.index == 2{
+                    SettingsView()
+                }
+                else if self.index == 3{
+                    Color.pink.edgesIgnoringSafeArea(.all)
+                    
+                    Text("Projects")
+                        .font(.largeTitle)
+                }
+                else{
+                    Color.red.opacity(0.5).edgesIgnoringSafeArea(.all)
+                    
+                    Text("Courses")
+                        .font(.largeTitle)
+                }
+                
+                
+                CustomTabBar(index: $index)
+            }
+           // .animation(.easeOut(duration: 0.5))
+        }
+
+//        TabView{
+//            HomeView2()
+//              .tabItem{
+//                  Image(systemName:"house")
+//                  Text("Home")
+//              }
+//
+//            InventoryListView(camera: MockData.sampleCamera, computer: MockData.sampleComputer, audio: MockData.sampleAudio, misc: MockData.sampleMisc)
+//                .tabItem{
+//                    Image(systemName:"list.bullet")
+//                    Text("Equipment")
+//                }
+//
+//
+//           SettingsView()
+//              .tabItem{
+//                  Image(systemName:"gear")
+//                  Text("Settings")
+//            }
+//        }
+        
+//        ProjectsView()
+//              .tabItem{
+//                  Image(systemName:"note.text")
+//                  Text("Projects")
+//              }
+//
+//            CoursesView()
+//              .tabItem{
+//                  Image(systemName:"folder.fill")
+//                  Text("Courses")
+//            }
+    }
+}
+
+
+//
+
