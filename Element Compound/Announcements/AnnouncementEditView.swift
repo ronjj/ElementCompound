7fbec69ea96332a26e3d58fb40bb4727d3542575
ronjj <ronaldjabouin2004@gmail.com>
2021-06-12 10:35:27 -0400

    put some logic from the announcement edit view into the announcement view model

--- Element Compound/Announcements/AnnouncementEditView.swift
+++ Element Compound/Announcements/AnnouncementEditView.swift
@@ -12,13 +12,6 @@ struct AnnouncementEditView: View {
     
     @StateObject var viewModel = AnnoucnementViewModel()
     @Environment(\.presentationMode) var presentationMode
-    
-    
-    let ReceiverFCMToken = "fWM1lZGSxEvZtPtfkf-sZF:APA91bFsRypqHw9QLFIy6hFoe-h4QPvo7yh7NWULa8QbYm3H_eG8kKd0KL6vA9A71HjCIFjd2YYuzkbUWSQh2CAMkYuLGnpzTKDdG8Jz7PAoN0TYsMP_Ti4IAHe1aVY7C-ghf0QRiSMo"
-    let legacyServerKey = "AAAAnjP38Rg:APA91bH6k0y3FhWBKdRImuwXyt_jgm5u-0HedjcfX30j_P6RJ5Pdc_-cGEZA_jjXUeXcLy39YO4MnIB4nWqFy9FGlrxUhdz5kVLhwdKWYMF7rtkjccrjPE_A2tXAV_9F0RK6SVUyAPmc"
-    @State private var fcmTokenMessage = "fcmTokenMessage"
-    @State private var instanceIDTokenMessage = "instanceIDTokenMessage"
-
 
     
     var body: some View {
@@ -52,7 +45,7 @@ struct AnnouncementEditView: View {
                     
                     Button(action: {
                         handleDoneTapped()
-                        self.sendMessageTouser(to: ReceiverFCMToken, title: self.viewModel.announcement.title, body: self.viewModel.announcement.message)
+                        self.viewModel.sendMessageTouser(to: viewModel.ReceiverFCMToken, title: self.viewModel.announcement.title, body: self.viewModel.announcement.message)
                     }) {
                         Text("Send")
                     }
@@ -63,6 +56,10 @@ struct AnnouncementEditView: View {
         }
     }
     
+    func dismiss() {
+        presentationMode.wrappedValue.dismiss()
+    }
+    
     func handleCancelTapped() {
         dismiss()
     }
@@ -71,56 +68,7 @@ struct AnnouncementEditView: View {
         viewModel.save()
         dismiss()
     }
-    
-    func dismiss() {
-        presentationMode.wrappedValue.dismiss()
-    }
-    
-    func sendMessageTouser(to token: String, title: String, body: String) {
-        print("sendMessageTouser()")
-        let urlString = "https://fcm.googleapis.com/fcm/send"
-        let url = NSURL(string: urlString)!
-        let paramString: [String : Any] = ["to" : token,
-                                           "notification" : ["title" : title, "body" : body],
-                                           "data" : ["user" : "test_id"]
-        ]
-        let request = NSMutableURLRequest(url: url as URL)
-        request.httpMethod = "POST"
-        request.httpBody = try? JSONSerialization.data(withJSONObject:paramString, options: [.prettyPrinted])
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("key=\(legacyServerKey)", forHTTPHeaderField: "Authorization")
-        let task =  URLSession.shared.dataTask(with: request as URLRequest)  { (data, response, error) in
-            do {
-                if let jsonData = data {
-                    if let jsonDataDict  = try JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.allowFragments) as? [String: AnyObject] {
-                        NSLog("Received data:\n\(jsonDataDict))")
-                    }
-                }
-            } catch let err as NSError {
-                print(err.debugDescription)
-            }
-        }
-        task.resume()
-    }
-    
-    func handleLogTokenTouch() {
-        // [START log_fcm_reg_token]
-        let token = Messaging.messaging().fcmToken
-        print("FCM token: \(token ?? "")")
-        // [END log_fcm_reg_token]
-        self.fcmTokenMessage  = "Logged FCM token: \(token ?? "")"
 
-        // [START log_iid_reg_token]
-        InstanceID.instanceID().instanceID { (result, error) in
-          if let error = error {
-            print("Error fetching remote instance ID: \(error)")
-          } else if let result = result {
-            print("Remote instance ID token: \(result.token)")
-            self.instanceIDTokenMessage  = "Remote InstanceID token: \(result.token)"
-          }
-        }
-        // [END log_iid_reg_token]
-    }
     
 }
 struct AnnouncementEditView_Previews: PreviewProvider {
--- Element Compound/Announcements/AnnouncementViewModel.swift
+++ Element Compound/Announcements/AnnouncementViewModel.swift
@@ -13,6 +13,11 @@ import SwiftUI
 class AnnoucnementViewModel: ObservableObject{
     @Published var announcement: Announcement = Announcement(message: "", title: "",  photoURL:(Auth.auth().currentUser?.photoURL!)!,  sender: Auth.auth().currentUser?.displayName ?? "N/A",  /*color: Color.blue,*/ dateEvent: Date())
     @Published var modified = false
+  
+    let ReceiverFCMToken = "fWM1lZGSxEvZtPtfkf-sZF:APA91bFsRypqHw9QLFIy6hFoe-h4QPvo7yh7NWULa8QbYm3H_eG8kKd0KL6vA9A71HjCIFjd2YYuzkbUWSQh2CAMkYuLGnpzTKDdG8Jz7PAoN0TYsMP_Ti4IAHe1aVY7C-ghf0QRiSMo"
+    let legacyServerKey = "AAAAnjP38Rg:APA91bH6k0y3FhWBKdRImuwXyt_jgm5u-0HedjcfX30j_P6RJ5Pdc_-cGEZA_jjXUeXcLy39YO4MnIB4nWqFy9FGlrxUhdz5kVLhwdKWYMF7rtkjccrjPE_A2tXAV_9F0RK6SVUyAPmc"
+    @State private var fcmTokenMessage = "fcmTokenMessage"
+    @State private var instanceIDTokenMessage = "instanceIDTokenMessage"
     
     private var db = Firestore.firestore()
     
@@ -41,83 +46,53 @@ class AnnoucnementViewModel: ObservableObject{
     func save() {
         addAnnouncement(announcement: announcement)
     }
+
+    func sendMessageTouser(to token: String, title: String, body: String) {
+        print("sendMessageTouser()")
+        let urlString = "https://fcm.googleapis.com/fcm/send"
+        let url = NSURL(string: urlString)!
+        let paramString: [String : Any] = ["to" : token,
+                                           "notification" : ["title" : title, "body" : body],
+                                           "data" : ["user" : "test_id"]
+        ]
+        let request = NSMutableURLRequest(url: url as URL)
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject:paramString, options: [.prettyPrinted])
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("key=\(legacyServerKey)", forHTTPHeaderField: "Authorization")
+        let task =  URLSession.shared.dataTask(with: request as URLRequest)  { (data, response, error) in
+            do {
+                if let jsonData = data {
+                    if let jsonDataDict  = try JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.allowFragments) as? [String: AnyObject] {
+                        NSLog("Received data:\n\(jsonDataDict))")
+                    }
+                }
+            } catch let err as NSError {
+                print(err.debugDescription)
+            }
+        }
+        task.resume()
+    }
+    
+    func handleLogTokenTouch() {
+        // [START log_fcm_reg_token]
+        let token = Messaging.messaging().fcmToken
+        print("FCM token: \(token ?? "")")
+        // [END log_fcm_reg_token]
+        self.fcmTokenMessage  = "Logged FCM token: \(token ?? "")"
+
+        // [START log_iid_reg_token]
+        InstanceID.instanceID().instanceID { (result, error) in
+          if let error = error {
+            print("Error fetching remote instance ID: \(error)")
+          } else if let result = result {
+            print("Remote instance ID token: \(result.token)")
+            self.instanceIDTokenMessage  = "Remote InstanceID token: \(result.token)"
+          }
+        }
+    }
 }
 
-
-
-//let ReceiverFCMToken = "fWM1lZGSxEvZtPtfkf-sZF:APA91bFsRypqHw9QLFIy6hFoe-h4QPvo7yh7NWULa8QbYm3H_eG8kKd0KL6vA9A71HjCIFjd2YYuzkbUWSQh2CAMkYuLGnpzTKDdG8Jz7PAoN0TYsMP_Ti4IAHe1aVY7C-ghf0QRiSMo"
-//let legacyServerKey = "AAAAnjP38Rg:APA91bH6k0y3FhWBKdRImuwXyt_jgm5u-0HedjcfX30j_P6RJ5Pdc_-cGEZA_jjXUeXcLy39YO4MnIB4nWqFy9FGlrxUhdz5kVLhwdKWYMF7rtkjccrjPE_A2tXAV_9F0RK6SVUyAPmc"
-//
-//struct AnnouncementViewModelStruct: View {
-//    @State private var fcmTokenMessage = "fcmTokenMessage"
-//    @State private var instanceIDTokenMessage = "instanceIDTokenMessage"
-//    
-//    @State private var notificationTitle: String = ""
-//    @State private var notificationContent: String = ""
-//    var body: some View {
-//        VStack {
-//            Text(fcmTokenMessage).padding(20)
-//            Text(instanceIDTokenMessage).padding(20)
-//            Button(action: {self.handleLogTokenTouch()}) {
-//                Text("Get user FCM Token String").font(.title)
-//            }.padding(20)
-//            TextField("Add Notification Title", text: $notificationTitle).textFieldStyle(RoundedBorderTextFieldStyle()).padding(20)
-//            TextField("Add Notification Content", text: $notificationContent).textFieldStyle(RoundedBorderTextFieldStyle()).padding(20)
-//            Button(action: {self.sendMessageTouser(to: ReceiverFCMToken, title: self.notificationTitle, body: self.notificationContent)
-//                self.notificationTitle = ""
-//                self.notificationContent = ""
-//            }) {
-//                Text("Send message to User").font(.title)
-//            }.padding(20)
-//        }
-//    }
-//    
-//    func sendMessageTouser(to token: String, title: String, body: String) {
-//        print("sendMessageTouser()")
-//        let urlString = "https://fcm.googleapis.com/fcm/send"
-//        let url = NSURL(string: urlString)!
-//        let paramString: [String : Any] = ["to" : token,
-//                                           "notification" : ["title" : title, "body" : body],
-//                                           "data" : ["user" : "test_id"]
-//        ]
-//        let request = NSMutableURLRequest(url: url as URL)
-//        request.httpMethod = "POST"
-//        request.httpBody = try? JSONSerialization.data(withJSONObject:paramString, options: [.prettyPrinted])
-//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.setValue("key=\(legacyServerKey)", forHTTPHeaderField: "Authorization")
-//        let task =  URLSession.shared.dataTask(with: request as URLRequest)  { (data, response, error) in
-//            do {
-//                if let jsonData = data {
-//                    if let jsonDataDict  = try JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.allowFragments) as? [String: AnyObject] {
-//                        NSLog("Received data:\n\(jsonDataDict))")
-//                    }
-//                }
-//            } catch let err as NSError {
-//                print(err.debugDescription)
-//            }
-//        }
-//        task.resume()
-//    }
-//    
-//    func handleLogTokenTouch() {
-//        // [START log_fcm_reg_token]
-//        let token = Messaging.messaging().fcmToken
-//        print("FCM token: \(token ?? "")")
-//        // [END log_fcm_reg_token]
-//        self.fcmTokenMessage  = "Logged FCM token: \(token ?? "")"
-//
-//        // [START log_iid_reg_token]
-//        InstanceID.instanceID().instanceID { (result, error) in
-//          if let error = error {
-//            print("Error fetching remote instance ID: \(error)")
-//          } else if let result = result {
-//            print("Remote instance ID token: \(result.token)")
-//            self.instanceIDTokenMessage  = "Remote InstanceID token: \(result.token)"
-//          }
-//        }
-//        // [END log_iid_reg_token]
-//    }
-//}
 
 
 
